b8aa0c5ef83120579ae6f1698f95edd762706742
MD AZAD ALI SHAH <46038479+sheikhazad@users.noreply.github.com>
2026-02-08 05:12:09 +0000

    Update comments for TradeData alignment
    Clarified comments regarding cache alignment in TradeData struct.

--- MyAlignedAllocator.cpp
+++ MyAlignedAllocator.cpp
@@ -3,8 +3,17 @@
 #include <iostream>
 #include <vector>
 
+//struct alignas(CACHE_LINE_SIZE) TradeData ==> Same as align first member of struct.
 struct TradeData {
-    alignas(CACHE_LINE_SIZE) int volume;
+    /*alignas(CACHE_LINE_SIZE) is still needed even when using a cache‑aligned allocator
+      MyAlignedAllocator guarantees that the start of the allocated block is aligned to a cache line.
+      But it does NOT guarantee that each element inside the block is cache‑line aligned.
+    */
+    //Same effect as struct alignas(CACHE_LINE_SIZE) TradeData
+    alignas(CACHE_LINE_SIZE) int volume; 
+    //aligning one member is enough to align the entire struct.
+    //There is no benefit to aligning rest of the struct members
+    //alignas(64) double price; // unnecessary
     double price;
 };
                 
